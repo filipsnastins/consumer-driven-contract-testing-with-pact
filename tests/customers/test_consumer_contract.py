867f8210dac7e52c0b8cacceadb4a4c71877a813
Filips Nastins <nastinsfilips@gmail.com>
2023-10-22 13:42:02 +0200

    WIP: remove Like

--- tests/customers/test_consumer_contract.py
+++ tests/customers/test_consumer_contract.py
@@ -13,7 +13,7 @@ from tests.pact_helpers import create_proto_from_pact
 
 @pytest.fixture()
 def pact() -> MessagePact:
-    return MessageConsumer("service-customers", version="0.0.2").has_pact_with(
+    return MessageConsumer("service-customers", version="0.0.1").has_pact_with(
         Provider("service-orders"),
         publish_to_broker=True,
         broker_base_url="http://localhost:9292",
@@ -33,17 +33,17 @@ async def service(mocker: MockerFixture) -> Service:
 
 @pytest.mark.asyncio()
 async def test_order_created(pact: MessagePact, service: Service) -> None:
-    expected_message = Like(
-        {
-            "correlation_id": Term(Format.Regexes.uuid.value, "23b2c005-a914-41ab-92d7-d8d4d8e98020"),
-            "customer_id": Term(Format.Regexes.uuid.value, "d3100f4f-c8a7-4207-a5e2-40aa122b4b33"),
-            "order_id": Term(Format.Regexes.uuid.value, "cc935616-e439-45a9-89ed-c6ef32bbc59e"),
-            "order_total": {
+    expected_message = {
+        "correlation_id": Term(Format.Regexes.uuid.value, "23b2c005-a914-41ab-92d7-d8d4d8e98020"),
+        "customer_id": Term(Format.Regexes.uuid.value, "d3100f4f-c8a7-4207-a5e2-40aa122b4b33"),
+        "order_id": Term(Format.Regexes.uuid.value, "cc935616-e439-45a9-89ed-c6ef32bbc59e"),
+        "order_total": Like(
+            {
                 "units": "100",
                 "nanos": 990000000,
-            },
-        }
-    )
+            }
+        ),
+    }
 
     pact.given("New order is created").expects_to_receive("OrderCreated event").with_content(expected_message)
     data = create_proto_from_pact(proto.OrderCreated, expected_message)
--- tests/orders/test_consumer_contract.py
+++ tests/orders/test_consumer_contract.py
@@ -3,7 +3,7 @@ import uuid
 import pytest
 import pytest_asyncio
 from fakes import InMemoryMessagePublisher
-from pact import Format, Like, MessageConsumer, MessagePact, Provider, Term
+from pact import Format, MessageConsumer, MessagePact, Provider, Term
 from pytest_mock import MockerFixture
 
 from adapters import proto
@@ -13,7 +13,7 @@ from tests.pact_helpers import create_proto_from_pact
 
 @pytest.fixture()
 def pact() -> MessagePact:
-    return MessageConsumer("service-orders", version="0.0.2").has_pact_with(
+    return MessageConsumer("service-orders", version="0.0.1").has_pact_with(
         Provider("service-customers"),
         publish_to_broker=True,
         broker_base_url="http://localhost:9292",
@@ -33,13 +33,11 @@ async def service(mocker: MockerFixture) -> Service:
 
 @pytest.mark.asyncio()
 async def test_customer_credit_reserved(pact: MessagePact, service: Service) -> None:
-    expected_message = Like(
-        {
-            "correlation_id": Term(Format.Regexes.uuid.value, "58b587a2-860c-4c4a-a9af-70457ffae596"),
-            "customer_id": Term(Format.Regexes.uuid.value, "1e5df855-a757-4aa5-a55f-2ddf6930b250"),
-            "order_id": Term(Format.Regexes.uuid.value, "f408cf27-8c53-486e-89f6-f0b45355b3ed"),
-        }
-    )
+    expected_message = {
+        "correlation_id": Term(Format.Regexes.uuid.value, "58b587a2-860c-4c4a-a9af-70457ffae596"),
+        "customer_id": Term(Format.Regexes.uuid.value, "1e5df855-a757-4aa5-a55f-2ddf6930b250"),
+        "order_id": Term(Format.Regexes.uuid.value, "f408cf27-8c53-486e-89f6-f0b45355b3ed"),
+    }
     (
         pact.given("Customer credit is reserved for created order")
         .expects_to_receive("CustomerCreditReserved event")
